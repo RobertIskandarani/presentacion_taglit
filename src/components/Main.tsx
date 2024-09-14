f755fb071f8a62ce3177e00934e9f0412859bb5d
Robert-Iskandarani <riskandarani@fi.uba.ar>
2024-09-14 20:22:05 -0300

    Feat: Se agrega qr

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,6 +9,7 @@ import { FaSpaceShuttle } from "react-icons/fa";
 
 import { useState } from "react";
 import DescriptionCard from "./DescriptionCard";
+import React from "react";
 
 const sectionsCards: CardProps[] = [
     {
@@ -114,25 +115,26 @@ export default function Main() {
 
     return (
         <main className={`w-full h-screen ${sectionsInfo[section].backgroundColor}`}>
-            <header className="w-full p-5">
+            <header className="w-full p-5 flex gap-20">
                 <h1 className="rubik-doodle-shadow-regular text-5xl whitespace-pre-line">{sectionsInfo[section].title}</h1>
+                <img src="/assets/qrToPage.png" alt="" className="hidden md:flex w-96 absolute top-40 left-1/2 -translate-x-1/2 border-4"/>
             </header>
             <section className="w-full flex flex-col gap-10 p-5">
-                {section === 0 && <h2 className=" text-2xl">Conoceme</h2>}
+                {section === 0 && <h2 className="md:hidden text-2xl">Conoceme</h2>}
                 {section === 0 ? 
-                    <section className="w-full h-40 flex justify-between items-center">
+                    <section className="w-full md:hidden h-40 flex justify-between items-center">
                         { 
                             sectionsCards.map(card =>
                                 <Card key={card.id} icon={card.icon} backgroundColor={sectionsInfo[section].backgroundColor} title={card.title} onClick={() => handleClick(card.id!)} />
                             )
                         }
                     </section> :
-                    <section className="w-full h-96 flex justify-between items-center">
+                    <section className="w-full md:hidden h-96 flex justify-between items-center">
                         <DescriptionCard src={subSectionsInfo[section - 1][subSection - 1].src} title={subSectionsInfo[section - 1][subSection - 1].title} backgroundColor={sectionsInfo[section].backgroundColor}/>
                     </section>
                 }
             </section>
-            <footer className="fixed bottom-0 left-0 w-full">
+            <footer className="fixed md:hidden bottom-0 left-0 w-full">
                 <span className={`${section !== 0 ? 'translate-x-[-500px]' : ''} cloud transition-all duration-500 absolute -bottom-1 -left-16 drop-shadow-[4px_4px_16px_rgba(0,0,0,.25)]`}>
                     <FaCloud color='white' size={350} />
                 </span>
@@ -143,16 +145,16 @@ export default function Main() {
                     <FaCloud color='white' size={350} />
                 </span>
                 <span className={`${section !== 1 ? 'translate-y-[500px]' : subSection === 1 ? '' : subSection === 2 ? 'rotate-90' : 'rotate-180'} transition-all duration-[750ms] w-full absolute -bottom-[195px]`}>
-                    <MdSunny size={390} color="#FFC800" />
+                    <MdSunny className="w-full h-auto" color="#FFC800" />
                 </span>
                 <span className={`${section !== 2 || subSection !== 1 ? 'translate-y-[500px]' : ''} transition-all duration-[750ms] w-full absolute -bottom-[195px]`}>
-                    <WiMoonWaningCrescent3 size={390} color="white" />
+                    <WiMoonWaningCrescent3 className="w-full h-auto" color="white" />
                 </span>
                 <span className={`${section !== 2 || subSection !== 2 ? 'translate-y-[500px]' : ''} transition-all duration-[750ms] w-full absolute -bottom-[195px]`}>
-                    <WiMoonFull size={390} color="white" />
+                    <WiMoonFull className="w-full h-auto" color="white" />
                 </span>
                 <span className={`${section !== 2 || subSection !== 3 ? 'translate-y-[500px]' : ''} transition-all duration-[750ms] rotate-180 w-full absolute -bottom-[195px]`}>
-                    <WiMoonWaningCrescent3 size={390} color="white" />
+                    <WiMoonWaningCrescent3 className="w-full h-auto" color="white" />
                 </span>
                 <div className={`${section !== 3 ? 'translate-y-[500px]' : ''} ${subSection === 1 ? '-rotate-[45deg]' : subSection === 2 ? 'rotate-[45deg]' : 'rotate-[360deg]'} orbit-1 transition-transform duration-[750ms] w-full absolute bottom-0`}>
                     <span className="w-12 h-12 bg-[var(--bg-planets)] flex items-center justify-center p-1 absolute bottom-[160px] left-1/2 z-10">
