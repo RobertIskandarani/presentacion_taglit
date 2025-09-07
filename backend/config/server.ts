50e29160fc1b60d36b76757616d6bcf353c1d818
RobertIskandarani <robertiska2005@gmail.com>
2025-09-07 02:26:47 -0300

    updates

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -4,4 +4,8 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env(
+    'PUBLIC_URL',
+    `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`
+  ),
 });
